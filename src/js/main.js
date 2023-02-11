5b87f797d751c5070fff9ff03a17d759c0ed735d
lngodenziutep <lngodenzi@miners.utep.edu>
2023-02-11 13:14:01 -0700

    troubleshooting bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
+// Import our custom CSS
 import '../scss/styles.scss'
 
-import Alert from 'bootstrap/js/dist/alert'
-
-// or, specify which plugins you need:
-import { Tooltip, Toast, Popover } from 'bootstrap'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
